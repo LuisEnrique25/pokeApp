034aede6b0990c633bfef77aa94a3994091dac78
LuisEnrique25 <luisrg2225@gmail.com>
2023-06-20 16:23:40 -0600

    Agregue la animacion wiggle

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,15 @@ export default {
     extend: {
       animation: {
         'spin-slow': 'spin 5s linear infinite',
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        }
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
       }
     },
   },
